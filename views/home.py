31d8420195e869fb4043012dd4ef1d12cabb8d2c
jhonnyjk <usuario.ikigai@email.com>
2024-10-07 09:22:11 -0500

    Avance 07/10/2024

--- views/home.py
+++ views/home.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 # Título de la app con separación
 st.markdown("""
     <h1 style='text-align: center; margin-bottom: 20px;'>Hola, ¡bienvenido! 🐒</h1>
-    <h6 style='text-align: center; margin-bottom: 100px;'>Utiliza esta aplicación para generar tus reportes de manera sencilla. Utiliza las diferentes herramientas disponibles en el menú lateral</6>
+    <h6 style='text-align: center; margin-bottom: 80px;'>Utiliza esta aplicación para generar tus reportes de manera sencilla. Utiliza las diferentes herramientas disponibles en el menú lateral</6>
 """, unsafe_allow_html=True)
 
 # Cargar el HTML de partículas
